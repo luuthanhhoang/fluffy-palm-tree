b04a394ee1a00848e78ba7d091a2bd0c67296563
HOANG THANH LUU <luu.ht162602@sis.hust.edu.vn>
2020-02-14 00:10:50 +0700

    Add file Cat

--- Cat.js
+++ Cat.js
@@ -2,6 +2,16 @@ function Cat() {
 	this.stomatch = [];
 }
 
-function Mouse(Name) {
+function Mouse(name) {
 	// body...
+	this.name = name;
 }
+
+Cat.prototype.eat = function(mouse) {
+	this.stomatch.push(mouse);
+};
+
+var Tom = new Cat();
+var Jery = new Mouse('Jery');
+Tom.eat(Jery);
+console.log(Tom);
